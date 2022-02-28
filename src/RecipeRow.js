63872f6ee993f9cea65ba0a625dcf1c4f5408c7d
Mad Godinez <madgodinez@gmail.com>
2022-02-28 15:15:35 -0600

    trying again with new dependencies

--- src/RecipeRow.js
+++ src/RecipeRow.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./RecipeRow.css";
 function RecipeRow({recipe, removeRecipe}, key){
-  const {name, cuisine, photo, ingredients, preparation, actions} = recipe;
+  const {name, cuisine, photo, ingredients, preparation} = recipe;
   return(
     <tr key={key}>
       <td className="name">{name}</td>
       <td className="cuisine">{cuisine}</td>
       <td className="photo">
-        <img src={photo} />
+        <img src={photo} alt="food from recipe" />
       </td>
       <td className="ingredients">{ingredients}</td>
       <td className="preparation">{preparation}</td>
